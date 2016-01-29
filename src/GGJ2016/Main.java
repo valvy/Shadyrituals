a892273319744511587337335621f6ec9fd7b9f7
Heiko van der Heijden <heikovanderheijden@Heikos-MacBook-Pro.local>
2016-01-30 00:34:54 +0100

    started durp

--- src/GGJ2016/Main.java
+++ src/GGJ2016/Main.java
@@ -9,11 +9,13 @@ public class Main{
 
     public static void main(String[] args) {
         //Dirty hack :-(
-        //Graphics.initializeGraphics(Graphics.GL_VERSION.GL_41);
+        ConnectionController con = new ConnectionController(true);
         if(System.getProperty("os.name").equals("Mac OS X")){
             System.setProperty("java.awt.headless", "true");//Otherwise freezes on os x :-(
         }
         Application.getInstance().loadScene(new GameScene());
+   
+       ;
     }
  
 }
