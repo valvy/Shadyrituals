61dffccebb02a076570a0785991cd28d83058b15
Heiko van der Heijden <heikovanderheijden@Heikos-MacBook-Pro.local>
2016-01-31 05:00:37 +0100

    shader

--- src/PrutEngine/Debug.java
+++ src/PrutEngine/Debug.java
@@ -30,7 +30,7 @@ package PrutEngine;
  * @author Heiko van der Heijden
  */
 public final class Debug {
-    private final static boolean DEBUG = true;
+    private final static boolean DEBUG = false;
     
     /**
      * Logs an message whenever debug is true
